16cdaedbf71c744462f13c3f65b1a9fc38c2e803
Dmitrij Fedorenko <c0va23@gmail.com>
2016-03-06 22:40:59 +0300

    Added optional support command "convert"

--- imageconverter.go
+++ imageconverter.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"gopkg.in/gographics/imagick.v2/imagick"
 )
 
 const imagesDir = "data/"
+
+var converter func(imageDate []byte) (output []byte, err error)
 
 func handler(responseWriter http.ResponseWriter, request *http.Request) {
 	imagePath := request.RequestURI[1:]
@@ -17,7 +23,7 @@ func handler(responseWriter http.ResponseWriter, request *http.Request) {
 	if imageData, fetchErr := fetchOriginImage(imagePath); nil != fetchErr {
 		log.Printf("Error fetch image date: %s", fetchErr)
 		responseWriter.WriteHeader(http.StatusNotFound)
-	} else if convertedImage, convertErr := convertImage(imageData); nil != convertErr {
+	} else if convertedImage, convertErr := converter(imageData); nil != convertErr {
 		log.Printf("Error convert image %s: %s", imagePath, convertErr)
 		responseWriter.WriteHeader(http.StatusInternalServerError)
 	} else if written, writeErr := responseWriter.Write(convertedImage); nil == writeErr {
@@ -31,7 +37,7 @@ func fetchOriginImage(imagePath string) ([]byte, error) {
 	return ioutil.ReadFile(imagesDir + imagePath)
 }
 
-func convertImage(imageData []byte) ([]byte, error) {
+func magickWandConvertImage(imageData []byte) ([]byte, error) {
 	magickWand := imagick.NewMagickWand()
 	defer magickWand.Destroy()
 
@@ -50,9 +56,47 @@ func convertImage(imageData []byte) ([]byte, error) {
 	return convertedImage, nil
 }
 
+func imageMagickCmdConvertImage(imageData []byte) ([]byte, error) {
+	cmd := exec.Command("convert", "-", "-resize", "800x600", "-")
+
+	cmd.Stdin = bytes.NewBuffer(imageData)
+
+	convertedImageData, runErr := cmd.Output()
+	if nil != runErr {
+		log.Printf("Error output: %s", runErr)
+	}
+	return convertedImageData, runErr
+}
+
+var converterName string
+
+const (
+	converterMagickwand  = "magickwand"
+	converterImagemagick = "imagemagick"
+)
+
+func init() {
+	flag.StringVar(&converterName, "converter", "magickwand", "Converter: magickwand or imagemagick")
+	flag.Parse()
+
+	log.Printf("Args: %v", os.Args)
+
+	switch converterName {
+	case converterMagickwand:
+		converter = magickWandConvertImage
+	case converterImagemagick:
+		converter = imageMagickCmdConvertImage
+	default:
+		log.Fatalf("Invalid converter: %s", converterName)
+	}
+	log.Printf("Used converter: %s", converterName)
+}
+
 func main() {
-	imagick.Initialize()
-	defer imagick.Terminate()
+	if converterName == converterMagickwand {
+		imagick.Initialize()
+		defer imagick.Terminate()
+	}
 
 	serverErr := http.ListenAndServe("[::]:5050", http.HandlerFunc(handler))
 	if nil != serverErr {
