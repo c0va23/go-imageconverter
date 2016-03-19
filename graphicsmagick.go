bf3933ae5fa47f57f92a33b57b8ee3f0a63bff31
Dmitrij Fedorenko <c0va23@gmail.com>
2016-03-19 13:40:17 +0300

    Use wand-api for graphicksmaigck

--- graphicsmagick.go
+++ graphicsmagick.go
@@ -7,12 +7,9 @@ import (
 	"unsafe"
 )
 
-/// # include <stdio.h>
-/// # include <string.h>
-/// # include <stdlib.h>
-
-// #cgo pkg-config: GraphicsMagick
-// #include <magick/api.h>
+// #cgo pkg-config: GraphicsMagickWand
+// #include <stdlib.h>
+// #include <wand/magick_wand.h>
 import "C"
 
 const converterName = "graphicsmagick"
@@ -26,37 +23,20 @@ func converterTerminate() {
 }
 
 func converter(imageDate []byte, width, height uint) (output []byte, err error) {
-	var exception C.ExceptionInfo
-	C.GetExceptionInfo(&exception)
-	defer C.DestroyExceptionInfo(&exception)
-
-	inImageInfo := C.CloneImageInfo(nil)
-	defer C.DestroyImageInfo(inImageInfo)
+	magickWand := C.NewMagickWand()
+	defer C.DestroyMagickWand(magickWand)
 
 	inBlob := unsafe.Pointer(&imageDate[0])
-	inLenght := C.size_t(len(imageDate))
-	inImage := C.BlobToImage(inImageInfo, inBlob, inLenght, &exception)
-	C.CatchException(&exception)
-	defer C.DestroyBlob(inImage)
-	defer C.DestroyImage(inImage)
+	inLenght := len(imageDate)
+	readBlobResult := C.MagickReadImageBlob(magickWand, (*C.uchar)(inBlob), C.size_t(inLenght))
+	log.Printf("MagickReadImageBlob: %v", readBlobResult)
 
-	outImage := C.ScaleImage(
-		inImage,
-		C.ulong(width),
-		C.ulong(height),
-		&exception,
-	)
-	C.CatchException(&exception)
-	defer C.DestroyImage(outImage)
-
-	outImageInfo := C.CloneImageInfo(inImageInfo)
-	defer C.DestroyImageInfo(outImageInfo)
+	scaleImageResult := C.MagickScaleImage(magickWand, C.ulong(width), C.ulong(height))
+	log.Printf("MagickScaleImage: %v", scaleImageResult)
 
 	var outLength C.size_t
-	var outBlob unsafe.Pointer
-	outBlob = C.ImageToBlob(outImageInfo, outImage, &outLength, &exception)
-	C.CatchException(&exception)
-	defer C.DestroyBlob(outImage)
+	outBlob := unsafe.Pointer(C.MagickWriteImageBlob(magickWand, &outLength))
+	defer C.free(outBlob)
 
 	outData := C.GoBytes(outBlob, C.int(outLength))
 
